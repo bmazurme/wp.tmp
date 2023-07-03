771b1bcacddcdf481145e98ea367497e5f871f07
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-03 08:08:20 +0300

    fix: ui-kit-page

--- src/pages/ui-kit/ui-kit-page.tsx
+++ src/pages/ui-kit/ui-kit-page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Content from '../../components/Content';
-import UiKit from '../../components/UiKit';
-import withUser from '../../hoc/withUser';
+import Content from '../../components/content';
+import UiKit from '../../layouts/ui-kit';
+import withUser from '../../hocs/with-user';
 
 function UiKitPage() {
   return (<Content children={<UiKit />} />);
